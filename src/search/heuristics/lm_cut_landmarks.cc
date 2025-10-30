9787924021343ef7015d51638288725850971212
Aeneas Meier <aeneas.meier@protonmail.com>
2025-10-30 15:44:14 +0100

    double tie breaking

--- src/search/heuristics/lm_cut_landmarks.cc
+++ src/search/heuristics/lm_cut_landmarks.cc
@@ -539,10 +539,10 @@ void LandmarkCutHMaxTieBreakExploration::update_supporters(RelaxedOperator &op) 
             op.heuristic_supporter = op.preconditions[i];
         else if (op.preconditions[i]->heuristic_cost == op.heuristic_supporter->heuristic_cost) {
             // Tie-break: prefer preconditions that is precondition of fewest operators.
-            if (op.preconditions[i]->precondition_of.size() < op.heuristic_supporter->precondition_of.size()) {
+            if (op.preconditions[i]->effect_of.size() < op.heuristic_supporter->effect_of.size()) {
                 op.heuristic_supporter = op.preconditions[i];
-            } else if (op.preconditions[i]->precondition_of.size() == op.heuristic_supporter->precondition_of.size()) {
-                if (op.effects.size() < op.heuristic_supporter->effect_of.size()) {
+            } else if (op.preconditions[i]->effect_of.size() == op.heuristic_supporter->effect_of.size()) {
+                if (op.effects.size() < op.heuristic_supporter->precondition_of.size()) {
                     op.heuristic_supporter = op.preconditions[i];
                 }
             }
